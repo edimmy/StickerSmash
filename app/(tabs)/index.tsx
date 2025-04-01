1526dcb15a21e34359a71a6189b1232cbc3b0da1
Emma Dimmig <81638584+edimmy@users.noreply.github.com>
2025-03-31 22:32:12 -0400

    working navigational tabs!!

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
@@ -16,6 +17,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
     color: '#fff',
   },
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import { LogBox } from "react-native";
 
-export default function RootLayout() {
-  return <Stack />;
+LogBox.ignoreAllLogs(true);
+
+export default function RootLayout(){
+    return(
+        <Stack>
+            <Stack.Screen 
+            name="(tabs)" 
+            options={{ 
+                headerShown: false,
+                }}/>
+            <Stack.Screen 
+            name="+not-found" 
+            options={{}}/>
+        </Stack>
+    );
 }
